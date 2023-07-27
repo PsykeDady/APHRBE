6384f80b9042f4af21690a7a39c792162fed7708
PsykeDady <psdady@msn.com>
2023-07-27 10:37:51 +0200

    ONDELETE Cascade added

--- src/main/java/co/psyke/aphr/entitites/Report.java
+++ src/main/java/co/psyke/aphr/entitites/Report.java
@@ -2,7 +2,12 @@ package co.psyke.aphr.entitites;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,11 +32,13 @@ public class Report {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id; 
 
-	@ManyToOne
+	@ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JoinColumn(name="employee_id", nullable = false)
 	private Employee employee; 
 
-	@ManyToOne
+	@ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JoinColumn(name="project_id", nullable = false)
 	private Project project; 
 
